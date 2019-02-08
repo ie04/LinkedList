d4ec4ff5ebc34b84797cedd90f8ac84b0b5553e1
ie04 <iyadeltifi12@gmail.com>
2019-02-08 17:43:43 -0500

    Created functional main file

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
-
+#include "LinkedList.hpp"
 int main() {
-    std::cout << "Dummy Main" << std::endl;
+    int userInt;
+    LinkedList ll;
+    Data* currData;
+
+    while(1){
+        std::cout << "Enter value, 0 to terminate: ";
+        std::cin >> userInt;
+
+        if(!userInt)
+            break;
+        
+        currData = new Data(userInt);
+
+        ll.insert(currData);
+
+    }
+
+    ll.show();
 }
